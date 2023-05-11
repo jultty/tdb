a96fe6f4278eba0d78888eacc014181bb8154d0f
jultty <juno.t@tuta.io>
2023-05-11 11:39:56 -0300

    Implement method arremessar

--- src/Jogo.ts
+++ src/Jogo.ts
@@ -7,15 +7,26 @@
   2. Pontuacao(): int - Chamado somente no fim do jogo
      Retorna um inteiro com o total do placar individual
 */
+import { Frame } from './frame.ts';
 
 export class Jogo {
+  score: number;
 
-  arremessar(): void {
-    throw new Error('Ainda não implementado');
+  constructor() {
+    this.score = 0;
+  }
+
+  arremessar(pinos: number): void {
+
+    if (pinos > 10 || pinos < 0 || isNaN(Number(pinos)))
+      throw new Error('Valor de arremesso inválido');
+    
+    this.score += pinos;
+    this.frame += 1;
   }
 
   pontuacao(): number {
-    return 0;
+    return this.score;
   }
 }
 
