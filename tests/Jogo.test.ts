6046f23ba3fa09cabfff643cb3714a7a7f08ae55
jultty <juno.t@tuta.io>
2023-05-11 11:39:56 -0300

    Implement method arremessar

--- tests/Jogo.test.ts
+++ tests/Jogo.test.ts
@@ -1,12 +1,46 @@
 import { describe, expect, test } from '@jest/globals';
 import { Jogo } from "../src/Jogo.ts";
 
-test('Pontuacao retorna zero', () => {
+test('Pontuacao inicial retorna zero', () => {
   const jogo = new Jogo();
   expect(jogo.pontuacao()).toBe(0);
 });
 
-test('Arremessar lança a exceção "Ainda não implementado"', () => {
+test('Derrubar 10 pinos incrementa o score em 10', () => {
   const jogo = new Jogo();
-  expect(() => jogo.arremessar()).toThrow('Ainda não implementado');
+  jogo.arremessar(10);
+  expect(jogo.score).toBe(10);
 });
+
+test('Derrubar 5 pinos incrementa o score em 5', () => {
+  const jogo = new Jogo();
+  jogo.arremessar(5);
+  expect(jogo.score).toBe(5);
+});
+
+test('Derrubar 5 pinos e depois 3 leva o score a 8', () => {
+  const jogo = new Jogo();
+  jogo.arremessar(5);
+  jogo.arremessar(3);
+  expect(jogo.score).toBe(8);
+});
+
+test('Um arremesso menor que 0 lança uma exceção e não afeta o score', () => {
+  const jogo = new Jogo();
+  expect(() => jogo.arremessar(-72)).toThrow('Valor de arremesso inválido');
+  expect(jogo.score).toBe(0);
+});
+
+test('Um arremesso maior que 10 lança uma exceção e não afeta o score', () => {
+  const jogo = new Jogo();
+  expect(() => jogo.arremessar(90)).toThrow('Valor de arremesso inválido');
+  expect(jogo.score).toBe(0);
+});
+
+test('Um arremesso com valor não-numérico lança uma exceção e não afeta o score', () => {
+  const jogo = new Jogo();
+  expect(() => jogo.arremessar('g')).toThrow('Valor de arremesso inválido');
+  expect(jogo.score).toBe(0);
+});
+
+
