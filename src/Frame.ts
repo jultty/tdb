23068465ce676dca9c4d7ee055861d3860bbb3c8
jultty <juno.t@tuta.io>
2023-05-15 22:59:36 -0300

    Refactor Frame and Jogo
    - Replaces repeated code
    - Extracts Frame-related code to the Frame class
    - Extracts validation to its own method

--- src/Frame.ts
+++ src/Frame.ts
@@ -1,11 +1,4 @@
 export class Frame {
-  numero: number
-  arremessos: number
-  pinosEmPe: number
-  strike: boolean
-  spare: boolean
-  bonus: number
-
   constructor(n: number) {
     this.numero = n
     this.arremessos = 0
@@ -13,6 +6,30 @@ export class Frame {
     this.strike = false
     this.spare = false
     this.bonus = 0
+  }
+
+  calcularBonus(pinos: number, frame: Frame):void {
+    if (this.arremessos === 0 && pinos === 10) {
+      this.strike = true
+      this.bonus = 2
+    }
+
+    if (this.arremessos === 1 && pinos === this.pinosEmPe) {
+      this.spare = true
+      this.bonus = 1
+    }
+  }
+
+  gravarArremesso(pinos: number): void {
+    this.arremessos += 1
+    this.pinosEmPe -= pinos
+  }
+
+  gerarProximoFrame(): Frame {
+    if (this.numero != 9 && (this.strike || this.arremessos === 2)) {
+      const proximoFrame: Frame = new Frame(this.numero + 1)
+      return proximoFrame
+    }
   }
 }
 
--- src/Jogo.ts
+++ src/Jogo.ts
@@ -1,18 +1,7 @@
-/*
-  Escreva uma classe chamada Jogo com dois métodos:
-
-  1. Arremessar (pinos: int): void - Chamado toda vez que fizerem uma jogada
-     O argumento é o número de pinos derrubados 
-
-  2. Pontuacao(): int - Chamado somente no fim do jogo
-     Retorna um inteiro com o total do placar individual
-*/
 import { Frame } from './Frame'
 
 export class Jogo {
-  score: number
   frames: Frame[] = []
-  frameAtual: number
 
   constructor() {
     this.score = 0
@@ -23,47 +12,44 @@ export class Jogo {
   }
 
   arremessar(pinos: number): void {
-
-    if (pinos > 10 || pinos < 0 || !Number.isInteger(pinos))
-      throw new Error('Valor de arremesso inválido')
-
-    if (this.frameAtual != 9 && pinos > this.frames[this.frameAtual].pinosEmPe)
-      throw new Error('Valor total dos arremessos superior a 10')
-
-    if (this.frameAtual === 9 && this.frames[this.frameAtual].arremessos > 3) {
-      throw new Error('O jogo já foi encerrado')
-    }
-
-    if (this.frames[this.frameAtual].arremessos === 0 && pinos === 10) {
-      this.frames[this.frameAtual].strike = true
-      this.frames[this.frameAtual].bonus = 2
-    }
-
-    if (this.frames[this.frameAtual].arremessos === 1 && pinos === this.frames[this.frameAtual].pinosEmPe) {
-      this.frames[this.frameAtual].spare = true
-      this.frames[this.frameAtual].bonus = 1
-    }
     
-    this.frames[this.frameAtual].arremessos += 1
-    this.frames[this.frameAtual].pinosEmPe -= pinos
+    const frame = this.frames[this.frameAtual]
+
+    this.validarArremesso(pinos, frame)
+    frame.calcularBonus(pinos)
+    frame.gravarArremesso(pinos)
+
     this.score += pinos
 
-    if (this.frameAtual != 9 && pinos === 10 && this.frames[this.frameAtual].arremessos === 1) {
+    if (this.frameAtual != 9 && frame.strike) {
       this.frameAtual += 1
-      const frame: Frame = new Frame(this.frameAtual)
-      this.frames.push(frame)
+      const proximoFrame: Frame = new Frame(this.frameAtual)
+      this.frames.push(proximoFrame)
       return
     }
 
-    if (this.frameAtual != 9 && this.frames[this.frameAtual].arremessos === 2) {
+    if (frame.gerarProximoFrame(1) instanceof Frame) {
       this.frameAtual += 1
-      const frame: Frame = new Frame(this.frameAtual)
-      this.frames.push(frame)
+      this.frames.push(frame.gerarProximoFrame())
     }
+
   }
 
   pontuacao(): number {
     return this.score
   }
+
+  validarArremesso(pinos: number, frame: Frame): void {
+    if (pinos > 10 || pinos < 0 || !Number.isInteger(pinos))
+      throw new Error('Valor de arremesso inválido')
+
+    if (frame.numero != 9 && pinos > frame.pinosEmPe)
+      throw new Error('Valor total dos arremessos superior a 10')
+
+    if (frame.numero === 9 && frame.arremessos > 3) {
+      throw new Error('O jogo já foi encerrado')
+    }
+  }
+
 }
 
